c34d3dca266e3f54b3b6051a506619a5250620bb
Chris Sibbitt <csibbitt@redhat.com>
2020-01-29 16:37:57 -0500

    Added operator-sdk version check

--- build/build.sh
+++ build/build.sh
@@ -5,4 +5,12 @@ REL=$(dirname "$0"); source "${REL}/metadata.sh"
 # FATA[0000] must run command in project root dir: project structure requires build/Dockerfile
 cd "${REL}/.."
 
+echo -e "\n* [info] Checking operator-sdk version\n"
+SDK_VERSION=$(operator-sdk version | cut -d '"' -f2)
+if [ "${SDK_VERSION}" != "${REQUIRED_OPERATOR_SDK_VERSION}" ]; then
+    echo -e "* [error] Wrong operator-sdk version. Wanted ${REQUIRED_OPERATOR_SDK_VERSION}, Got: ${SDK_VERSION}\n";
+    exit 1
+fi
+
+
 operator-sdk build "${OPERATOR_NAME}:${IMAGE_TAG}" --image-builder "${IMAGE_BUILDER}"
---
+++ ci.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+echo "Passing CI with this thorough test"
+
