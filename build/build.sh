8fa2100ca29fda925de616d51760c33edd8c4846
Chris Sibbitt <csibbitt@redhat.com>
2020-02-11 16:47:34 -0500

    Switched to build from local checkout

--- build/build.sh
+++ build/build.sh
@@ -11,4 +11,4 @@ oc create -f <(sed "
     s|<<OCP_TAG>>|${OCP_TAG}|g"\
     "buildConfig.yaml.template")
 
-oc start-build "${OPERATOR_NAME}" --wait --follow
+oc start-build "${OPERATOR_NAME}" --wait --follow --from-repo "${REL}/.."
