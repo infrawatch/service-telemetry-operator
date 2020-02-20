dca4c0a0381a2d284ae605231f34ed2648a72eb6
Leif Madsen <lmadsen@redhat.com>
2020-02-20 11:11:06 -0500

    Fix expected name for operator-sdk binary

--- build/travis/after_success.sh
+++ build/travis/after_success.sh
@@ -8,11 +8,11 @@ REL=$(dirname "$0")
 # Add everything, get ready for commit. But only do it if we're on master or a
 # named branch of the format <#>.<#>.X. If you want to deploy on different
 # branches, you can change this.
-if [[ "$BRANCH" =~ ^mazter$|^[0-9]+\.[0-9]+\.X$ ]]; then
+if [[ "$BRANCH" =~ ^master$|^[0-9]+\.[0-9]+\.X$ ]]; then
     echo "Branch is master, so push new application to Quay registry"
     export PATH=$HOME/bin:$PATH
-    curl -L https://github.com/operator-framework/operator-sdk/releases/download/v0.12.0/operator-sdk-v0.12.0-x86_64-linux-gnu -o $HOME/bin/operator-sdk-v0.12.0
-    chmod +x $HOME/bin/operator-sdk-v0.12.0
+    curl -L https://github.com/operator-framework/operator-sdk/releases/download/v0.12.0/operator-sdk-v0.12.0-x86_64-linux-gnu -o $HOME/bin/operator-sdk
+    chmod +x $HOME/bin/operator-sdk
     operator-courier verify --ui_validate_io deploy/olm-catalog/service-telemetry-operator
     ansible-lint roles/servicetelemetry/
     ${REL}/../update_csv.sh
