12000b52eb0dccda7a80a90d56033c53913b6deb
Leif Madsen <lmadsen@redhat.com>
2020-04-27 16:53:04 -0400

    Adjust how infrared is used in test scripts
    Adjust the test script so that every step is a separate bash function. This will allow us some
    additional controls in the future should it be wanted, but for now it's just allowing a new
    environment variable to be passed called TEMPEST_ONLY accepting true/false values.
    
    Originally I wanted this setup so that I could re-run the tempest tests to provide some
    sort of load on the cloud for additional STF testing and possibly in the future running
    a specific set of tests that might result in expected alarms etc firing off (which could
    test the end-to-end transit).
    
    Additinally the metrics-qdr-connectors.yaml has been renamed to better reflect the upstream
    documentation, so the file is now stf-connectors.yaml.

--- tests/infrared/16/infrared-openstack.sh
+++ tests/infrared/16/infrared-openstack.sh
@@ -22,65 +22,92 @@ OSP_TOPOLOGY="${OSP_TOPOLOGY:-undercloud:1,controller:1,compute:1,ceph:1}"
 OSP_MIRROR="${OSP_MIRROR:-rdu2}"
 LIBVIRT_DISKPOOL="${LIBVIRT_DISKPOOL:-/var/lib/libvirt/images}"
 
-infrared virsh \
-    -vv \
-    -o outputs/cleanup.yml \
-    --disk-pool "${LIBVIRT_DISKPOOL}" \
-    --host-address "${VIRTHOST}" \
-    --host-key "${SSH_KEY}" \
-    --cleanup yes
+TEMPEST_ONLY="${TEMPEST_ONLY:-false}"
 
-infrared virsh \
-    -vvv \
-    -o outputs/provision.yml \
-    --disk-pool /home/libvirt/images \
-    --topology-nodes "${OSP_TOPOLOGY}" \
-    --host-address "${VIRTHOST}" \
-    --host-key "${SSH_KEY}" \
-    --image-url "${VM_IMAGE_LOCATION}" \
-    --host-memory-overcommit True \
-    -e override.controller.cpu=8 \
-    -e override.controller.memory=32768 \
-    --serial-files True
+ir_run_cleanup() {
+  infrared virsh \
+      -vv \
+      -o outputs/cleanup.yml \
+      --disk-pool "${LIBVIRT_DISKPOOL}" \
+      --host-address "${VIRTHOST}" \
+      --host-key "${SSH_KEY}" \
+      --cleanup yes
+}
 
-infrared tripleo-undercloud \
-    -vv \
-    -o outputs/undercloud-install.yml \
-    --mirror "${OSP_MIRROR}" \
-    --version ${OSP_VERSION} \
-    --build "${OSP_BUILD}" \
-    --images-task rpm \
-    --images-update no \
-    --tls-ca https://password.corp.redhat.com/RH-IT-Root-CA.crt \
-    --config-options DEFAULT.undercloud_timezone=UTC
+ir_run_provision() {
+  infrared virsh \
+      -vvv \
+      -o outputs/provision.yml \
+      --disk-pool /home/libvirt/images \
+      --topology-nodes "${OSP_TOPOLOGY}" \
+      --host-address "${VIRTHOST}" \
+      --host-key "${SSH_KEY}" \
+      --image-url "${VM_IMAGE_LOCATION}" \
+      --host-memory-overcommit True \
+      -e override.controller.cpu=8 \
+      -e override.controller.memory=32768 \
+      --serial-files True
+}
 
-sed -e "s/<<AMQP_HOST>>/${AMQP_HOST}/;s/<<AMQP_PORT>>/${AMQP_PORT}/" metrics-qdr-connectors.yaml.template > outputs/metrics-qdr-connectors.yaml
+ir_create_undercloud() {
+  infrared tripleo-undercloud \
+      -vv \
+      -o outputs/undercloud-install.yml \
+      --mirror "${OSP_MIRROR}" \
+      --version "${OSP_VERSION}" \
+      --build "${OSP_BUILD}" \
+      --images-task rpm \
+      --images-update no \
+      --tls-ca https://password.corp.redhat.com/RH-IT-Root-CA.crt \
+      --config-options DEFAULT.undercloud_timezone=UTC
+}
 
-infrared tripleo-overcloud \
-    -vv \
-    -o outputs/overcloud-install.yml \
-    --version ${OSP_VERSION} \
-    --deployment-files virt \
-    --overcloud-debug yes \
-    --network-backend geneve \
-    --network-protocol ipv4 \
-    --network-dvr yes \
-    --storage-backend ceph \
-    --storage-external no \
-    --overcloud-ssl no \
-    --introspect yes \
-    --tagging yes \
-    --deploy yes \
-    --ntp-server ${NTP_SERVER} \
-    --containers yes \
-    --overcloud-templates outputs/metrics-qdr-connectors.yaml
+stf_create_config() {
+  sed -e "s/<<AMQP_HOST>>/${AMQP_HOST}/;s/<<AMQP_PORT>>/${AMQP_PORT}/" stf-connectors.yaml.template > outputs/stf-connectors.yaml
+}
 
-infrared tempest \
-	-vv \
-    -o outputs/test.yml \
-    --openstack-installer tripleo \
-    --openstack-version "${OSP_VERSION}" \
-    --tests smoke \
-    --setup rpm \
-    --revision=HEAD \
-    --image http://download.cirros-cloud.net/0.4.0/cirros-0.4.0-x86_64-disk.img
+ir_create_overcloud() {
+  infrared tripleo-overcloud \
+      -vv \
+      -o outputs/overcloud-install.yml \
+      --version "${OSP_VERSION}" \
+      --deployment-files virt \
+      --overcloud-debug yes \
+      --network-backend geneve \
+      --network-protocol ipv4 \
+      --network-dvr yes \
+      --storage-backend ceph \
+      --storage-external no \
+      --overcloud-ssl no \
+      --introspect yes \
+      --tagging yes \
+      --deploy yes \
+      --ntp-server "${NTP_SERVER}" \
+      --containers yes \
+      --overcloud-templates outputs/stf-connectors.yaml
+}
+
+ir_run_tempest() {
+  infrared tempest \
+      -vv \
+      -o outputs/test.yml \
+      --openstack-installer tripleo \
+      --openstack-version "${OSP_VERSION}" \
+      --tests smoke \
+      --setup rpm \
+      --revision=HEAD \
+      --image http://download.cirros-cloud.net/0.4.0/cirros-0.4.0-x86_64-disk.img
+}
+
+if ${TEMPEST_ONLY}; then
+  echo "-- Running tempest tests"
+  ir_run_tempest
+else
+  echo "-- full cloud deployment"
+  ir_run_cleanup
+  ir_run_provision
+  ir_create_undercloud
+  stf_create_config
+  ir_create_overcloud
+  ir_run_tempest
+fi
