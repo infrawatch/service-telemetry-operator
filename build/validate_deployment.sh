e638b54dd2d7b8154eff84270b056af708ababe0
Leif Madsen <lmadsen@redhat.com>
2020-07-07 00:16:12 -0400

    Complete POC
    Complete initial implementation of an ansible role that replaces the existing
    bash scripts and CI deployment.
    
    This commit adds the validation script which is a stripped version of deploy_ci.sh
    with just the final environment validation left. Also completes the deployment
    of STF by creating a ServiceTelemetry object.
    
    Starts the clean up effort by removing old scripts that are now consumed by this
    Ansible role.

--- build/validate_deployment.sh
+++ build/validate_deployment.sh
@@ -1,27 +1,5 @@
 #!/usr/bin/env bash
-#
-# Entrypoint to build and deploy for CI
-#
 set -e
-REL=$(dirname "$0"); source "${REL}/../build/metadata.sh"
-
-# Running quickstart triggers an initial install via a subscription.
-# OLM satisfies dependencies via the subscription, allowing us to test our deps
-# are still working as well as creating a more DRY CI deployment.
-# We pass "nostf" config which supresses the creation of the ServiceTelemetry
-# object itself, effectively preventing the original operator from doing
-# "anything" (except establishing the CRD.... and...?)
-echo -e "\n* [info] Running quickstart...\n"
-QUICKSTART_CONFIG="configs/nostf.bash" "${REL}/quickstart.sh"
-
-# After quickstart, a CSV pointing at the upstream SAO image will be installed.
-# This script removes it and replaces it with the patched version
-echo -e "\n* [info] Re-deploying with local build...\n"
-"${REL}/deploy_local_build.sh"
-
-# Now we can install an STF object for the locally built operator to work on
-source "${REL}/${QUICKSTART_CONFIG}"
-oc create -f - <<< "${KIND_SERVICETELEMETRY}"
 
 # Play the (automated!) waiting game
 echo -e "\n* [info] Waiting for QDR deployment to complete\n"
