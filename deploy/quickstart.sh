78343b8fd0e4013bda8e5a7f1901b80c5fd5a68b
Leif Madsen <lmadsen@redhat.com>
2021-02-01 13:56:01 -0500

    Use new unstable channel for quickstart
    Use the new unstable channel for the quickstart.sh script. Also sneaks in a small
    feature that makes using quickstart.sh against systems without persistent storage
    easier (adds EPHERMAL_STORAGE environment variable).
    
    Closes #164

--- deploy/quickstart.sh
+++ deploy/quickstart.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 REL=$(dirname "$0"); source "${REL}/../build/metadata.sh"
+EPHEMERAL_STORAGE="${EPHEMERAL_STORAGE:-false}"
 
 oc new-project "${OCP_PROJECT}"
 ansible-playbook \
     --extra-vars namespace="${OCP_PROJECT}" \
     --extra-vars __local_build_enabled=false \
     --extra-vars __service_telemetry_snmptraps_enabled=true \
+    --extra-vars __service_telemetry_storage_ephemeral_enabled=${EPHEMERAL_STORAGE} \
     ${REL}/../build/run-ci.yaml
