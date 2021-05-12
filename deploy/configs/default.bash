b4b1e795e08de1670f7e80c5587d2de16fa9a425
Leif Madsen <lmadsen@redhat.com>
2021-05-12 16:33:37 -0400

    Drop deprecated infra.watch/v1alpha1 interface (#197)
    * Drop deprecated infra.watch/v1alpha1 interface
    
    Drop the infra.watch/v1alpha1 interface (CRD) from the repository.
    
    Resolves: rhbz#1960025
    
    * Adjust for single CRD version
    
    Adjust the CSV to account for a single CRD version vs multiple versions. Verify everything
    passes validation.

--- deploy/configs/default.bash
+++
@@ -1,8 +0,0 @@
-KIND_SERVICETELEMETRY="apiVersion: infra.watch/v1alpha1
-kind: ServiceTelemetry
-metadata:
-  name: default
-  namespace: ${OCP_PROJECT}
-spec:
-  metricsEnabled: true
-  eventsEnabled: true"
