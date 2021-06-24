33de662684a568d1349e3ec20373faea2abefa82
Paul Leimer <pleimer@redhat.com>
2021-06-24 15:26:55 -0400

    add agent resources

--- deploy/quickstart.sh
+++ deploy/quickstart.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 REL=$(dirname "$0"); source "${REL}/../build/metadata.sh"
-EPHEMERAL_STORAGE="${EPHEMERAL_STORAGE:-false}"
-
-oc new-project "${OCP_PROJECT}"
 ansible-playbook \
-    --extra-vars namespace="${OCP_PROJECT}" \
-    --extra-vars __local_build_enabled=false \
+    --extra-vars namespace="service-telemetry" \
+    --extra-vars __local_build_enabled=true \
     --extra-vars __service_telemetry_snmptraps_enabled=true \
-    --extra-vars __service_telemetry_storage_ephemeral_enabled=${EPHEMERAL_STORAGE} \
+    --extra-vars __service_telemetry_storage_ephemeral_enabled=true \
     ${REL}/../build/run-ci.yaml
