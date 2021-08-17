d609a86e3f016b1507caa0268f820927a7cf647c
Jaromír Wysoglad <jwysogla@redhat.com>
2021-08-17 08:06:44 +0200

    Make operator sdk version a variable

--- build/get_new_operator_sdk.sh
+++ build/get_new_operator_sdk.sh
@@ -3,10 +3,11 @@
 REL=$(dirname "$0")
 ARCH=$(case $(uname -m) in x86_64) echo -n amd64 ;; aarch64) echo -n arm64 ;; *) echo -n $(uname -m) ;; esac)
 OS=$(uname | awk '{print tolower($0)}')
-OPERATOR_SDK_DL_URL=https://github.com/operator-framework/operator-sdk/releases/download/v1.5.0
+VERSION="$1"
+OPERATOR_SDK_DL_URL=https://github.com/operator-framework/operator-sdk/releases/download/${VERSION}
 
-if [[ ! -f ${REL}/working/operator-sdk-v1.5.0 ]]; then
-	curl -L ${OPERATOR_SDK_DL_URL}/operator-sdk_${OS}_${ARCH} -o ${REL}/working/operator-sdk-v1.5.0
-	chmod +x ${REL}/working/operator-sdk-v1.5.0
+if [[ ! -f ${REL}/working/operator-sdk-${VERSION} ]]; then
+	curl -L ${OPERATOR_SDK_DL_URL}/operator-sdk_${OS}_${ARCH} -o ${REL}/working/operator-sdk-${VERSION}
+	chmod +x ${REL}/working/operator-sdk-${VERSION}
 fi
 
