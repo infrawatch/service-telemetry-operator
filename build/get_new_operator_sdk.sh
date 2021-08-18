5b3f7f9fea73a8fde37a346e869fa981b271496c
Jaromír Wysoglad <jwysogla@redhat.com>
2021-08-18 08:15:11 +0200

    Add default version to get_new_operator_sdk.sh

--- build/get_new_operator_sdk.sh
+++ build/get_new_operator_sdk.sh
@@ -3,7 +3,7 @@
 REL=$(dirname "$0")
 ARCH=$(case $(uname -m) in x86_64) echo -n amd64 ;; aarch64) echo -n arm64 ;; *) echo -n $(uname -m) ;; esac)
 OS=$(uname | awk '{print tolower($0)}')
-VERSION="$1"
+VERSION="${1:-v1.5.0}"
 OPERATOR_SDK_DL_URL=https://github.com/operator-framework/operator-sdk/releases/download/${VERSION}
 
 if [[ ! -f ${REL}/working/operator-sdk-${VERSION} ]]; then
