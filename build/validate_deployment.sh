b8e68fda12aa3398ee51169db728729759020dd6
Leif Madsen <lmadsen@redhat.com>
2021-11-18 15:29:24 -0500

    Add observabilityStrategy option of none (#294)
    * Add observabilityStrategy option of none
    
    Add observabilityStrategy of none which will result in no observability
    components being deployed. Previously this option was configured as
    backends.deploymentStrategy which was confusing. This change moves
    backends.deploymentStrategy to a top level object, and renames it to
    observabilityStrategy.
    
    Closes: STF-458
    
    * Implement observability strategy for CI
    
    Implement a method to deploy with observabilityStrategy: none with the
    CI tooling.
    
    * Fix syntax in ansible output
    
    * observabilityStrategy is now a top level parameter
    
    Configure the observabilityStrategy parameter as a top level object. To
    do this, it needs to be defined in roles/.../defaults/main.yml at the
    top of the file (like cloudsRemoveOnMissing).
    
    For top level parameters, if they are not a mapping, then they are not
    defined in roles/.../vars/main.yml and can be defined directly in the
    defaults configuration. Tasks then were updated to use the top-level
    parameter name vs in the servicetelemetry_vars map.
    
    * Add missing default configuration
    
    * Adjustments to control logic
    
    * Check for certmanager API for QDR
    
    Check for certmanager API interface for QDR deployment since it requests
    the creation of a certificate. Remove a superfluous check for
    certmanager as it's already verified in the outer block condition
    checks.
    
    * Add VALIDATION_SCOPE var for validation script
    
    Add a scope control for validate_deployment.sh that allows for an
    observabilityStrategy value of 'none' to still pass validation
    
    * Add additional CI controls for observabilityStrategy none

--- build/validate_deployment.sh
+++ build/validate_deployment.sh
@@ -1,30 +1,44 @@
 #!/usr/bin/env bash
 set -e
 
-if [ -n ${OCP_PROJECT+x} ]; then
-    oc project $OCP_PROJECT
+if [ -n "${OCP_PROJECT+x}" ]; then
+    oc project "$OCP_PROJECT"
 fi
+
+VALIDATION_SCOPE="${VALIDATION_SCOPE:-use_community}"
 
 # Play the (automated!) waiting game
 echo -e "\n* [info] Waiting for QDR deployment to complete\n"
 until timeout 300 oc rollout status deployment.apps/default-interconnect; do sleep 3; done
-echo -e "\n* [info] Waiting for prometheus deployment to complete\n"
-until timeout 300 oc rollout status statefulset.apps/prometheus-default; do sleep 3; done
-echo -e "\n* [info] Waiting for elasticsearch deployment to complete \n"
-while true; do
-    sleep 3
-    ES_READY=$(oc get statefulsets elasticsearch-es-default -ogo-template='{{ .status.readyReplicas }}') || continue
-    if [ "${ES_READY}" == "1" ]; then
-        break
-    fi
-done
-echo -e "\n* [info] Waiting for alertmanager deployment to complete\n"
-until timeout 300 oc rollout status statefulset.apps/alertmanager-default; do sleep 3; done
-echo -e "\n* [info] Waiting for smart-gateway deployment to complete\n"
-until timeout 300 oc rollout status deployment.apps/default-cloud1-coll-meter-smartgateway; do sleep 3; done
-until timeout 300 oc rollout status deployment.apps/default-cloud1-coll-event-smartgateway; do sleep 3; done
-until timeout 300 oc rollout status deployment.apps/default-cloud1-ceil-event-smartgateway; do sleep 3; done
-until timeout 300 oc rollout status deployment.apps/default-cloud1-ceil-meter-smartgateway; do sleep 3; done
+
+case "${VALIDATION_SCOPE}" in
+    "use_community")
+        echo -e "\n* [info] Waiting for prometheus deployment to complete\n"
+        until timeout 300 oc rollout status statefulset.apps/prometheus-default; do sleep 3; done
+        echo -e "\n* [info] Waiting for elasticsearch deployment to complete \n"
+        while true; do
+            sleep 3
+            ES_READY=$(oc get statefulsets elasticsearch-es-default -ogo-template='{{ .status.readyReplicas }}') || continue
+            if [ "${ES_READY}" == "1" ]; then
+                break
+            fi
+        done
+        echo -e "\n* [info] Waiting for alertmanager deployment to complete\n"
+        until timeout 300 oc rollout status statefulset.apps/alertmanager-default; do sleep 3; done
+        echo -e "\n* [info] Waiting for smart-gateway deployment to complete\n"
+        until timeout 300 oc rollout status deployment.apps/default-cloud1-coll-meter-smartgateway; do sleep 3; done
+        until timeout 300 oc rollout status deployment.apps/default-cloud1-coll-event-smartgateway; do sleep 3; done
+        until timeout 300 oc rollout status deployment.apps/default-cloud1-ceil-event-smartgateway; do sleep 3; done
+        until timeout 300 oc rollout status deployment.apps/default-cloud1-ceil-meter-smartgateway; do sleep 3; done
+    ;;
+
+    "none")
+        echo -e "\n* [info] Waiting for smart-gateway deployment to complete\n"
+        until timeout 300 oc rollout status deployment.apps/default-cloud1-coll-meter-smartgateway; do sleep 3; done
+        until timeout 300 oc rollout status deployment.apps/default-cloud1-ceil-meter-smartgateway; do sleep 3; done
+    ;;
+esac
+
 echo -e "\n* [info] Waiting for all pods to show Ready/Complete\n"
 while oc get pods --selector '!openshift.io/build.name' | tail -n +2 | grep -v -E 'Running|Completed'; do
     sleep 3
