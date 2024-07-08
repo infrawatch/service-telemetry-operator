1ac352dd8c92f083fef8f065525fcbcaab57396a
Victoria Martinez de la Cruz <victoria@redhat.com>
2024-07-08 13:46:49 +0200

    Revert "Set a default version for opm"
    This reverts commit d6a51df8b2b5883e19a29c00abd1b0d6286c53af.

--- build/get_opm.sh
+++ build/get_opm.sh
@@ -6,11 +6,11 @@ REL=$(dirname "$0")
 VERSION="$1"
 OPM_DL_URL=https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/${VERSION}
 
-if [[ ! -f ${REL}/working/opm ]]; then
+if [[ ! -f ${REL}/working/opm-${VERSION} ]]; then
 	mkdir -p ${REL}/working
-	curl -L ${OPM_DL_URL}/opm-linux.tar.gz -o ${REL}/working/opm
-	chmod +x ${REL}/working/opm
-	rm -f ${REL}/working/opm-linux.tar.gz
+	curl -L ${OPM_DL_URL}/opm-linux-${VERSION}.tar.gz -o ${REL}/working/opm-${VERSION}
+	chmod +x ${REL}/working/opm-${VERSION}
+	rm -f ${REL}/working/opm-linux-${VERSION}.tar.gz
 fi
 
 set +x
